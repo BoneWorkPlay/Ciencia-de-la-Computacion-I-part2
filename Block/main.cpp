1687b497f429c274d712d83d5e467f3b6e672da4
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-16 09:20:52 -0500

    BLOCK

---
+++ Block/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cout<<"Ingrese X: ";
+    cin>>x;
+    cout<<"Ingrese Y: ";
+    cin>>y;
+    z=x+y;
+    cout<<"La suma es: "<<z<<endl;
+}
